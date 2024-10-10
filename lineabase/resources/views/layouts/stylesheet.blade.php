9e6ff204eae59ed1d30d334a42235b2cfdc0d824
Misael Sánchez <94056311+MisaelS1993@users.noreply.github.com>
2024-10-10 16:28:25 -0600

    correccion

--- lineabase/resources/views/layouts/stylesheet.blade.php
+++ lineabase/resources/views/layouts/stylesheet.blade.php
@@ -1,3 +1,6 @@
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Boleta Linea Base</title>
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fonts/Raleway-Regular.ttf') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
