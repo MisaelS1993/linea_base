421b014017f158a6366284cc8a71ef7738e95e1f
Misael Sánchez <94056311+MisaelS1993@users.noreply.github.com>
2024-10-10 21:44:25 -0600

    bootstrap agregado

--- lineabase/resources/views/layouts/scripts.blade.php
+++ lineabase/resources/views/layouts/scripts.blade.php
@@ -5,5 +5,8 @@
     <script src="{{ asset('assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{ asset('assets/js/pages/dashboard.js') }}"></script>
     <script src="{{ asset('assets/js/main.js') }}"></script>
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     @stack('modals')
     @livewireScripts
--- lineabase/resources/views/layouts/stylesheet.blade.php
+++ lineabase/resources/views/layouts/stylesheet.blade.php
@@ -2,6 +2,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Boleta Linea Base</title>
     <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fonts/Raleway-Regular.ttf') }}">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
     <link rel="stylesheet" href="{{ asset('assets/css/app.css') }}">
